c8086cd2231b7e32d9a6ee56c40b530fcc26b782
MoodyBlues04 <105989507+MoodyBlues04@users.noreply.github.com>
2024-03-25 17:39:59 +0300

    fix-api

--- app/services/ads_sheet_processor.py
+++ app/services/ads_sheet_processor.py
@@ -16,15 +16,16 @@ class AvitoSheetProcessor:
 
         for row_idx, row in enumerate(all_rows):
             try:
-                if len(row) > 0 and not row[0]:
+                if row_idx == 0 or len(row) > 0 and not row[0]:
                     continue
                 if len(row) < self.ROW_LEN:
                     raise Exception('Incorrect args count')
 
                 profile_id, client_id, client_secret = row[1:4]
 
-                auth_request = AuthRequest(client_id, client_secret)
+                auth_request = AuthRequest(client_id.strip(), client_secret.strip())
                 avito_api = AvitoApi(auth_request)
+                print(avito_api.get_ads('active'))
 
             #     1) acc data (balance, ads count, ads2 count, reviews count, rating, nearest ad,
             #           profile url and other profile data
@@ -41,4 +42,5 @@ class AvitoSheetProcessor:
                 self.__log_error(e, row_idx)
 
     def __log_error(self, e: Exception, row_index: int) -> None:
-        self.__google_sheets_api.set_value((row_index + 1, self.ERR_COLUMN), str(e))
+        raise e
+        # self.__google_sheets_api.set_value((row_index + 1, self.ERR_COLUMN), str(e))
--- app/services/avito.py
+++ app/services/avito.py
@@ -3,6 +3,8 @@ import requests
 
 
 class AuthRequest:
+    GRANT_TYPE = 'client_credentials'
+
     def __init__(self, client_id: str, client_secret: str) -> None:
         self.__client_id = client_id
         self.__client_secret = client_secret
@@ -23,6 +25,7 @@ class AvitoApi:
 
     def __init__(self, auth_request: AuthRequest) -> None:
         self.__auth_headers = self.__get_auth_headers(auth_request.get_request())
+        print(self.__auth_headers)
 
     def get_account(self) -> dict:
         return self.__get(self.CORE_API_HOST + "accounts/self", headers=self.__auth_headers)
@@ -37,7 +40,10 @@ class AvitoApi:
         page = 1
         ads = []
         while True:
-            response = self.__get(self.CORE_API_HOST + f'/items?per_page=100&page={page}&status={status}')
+            response = self.__get(
+                self.CORE_API_HOST + f'/items?per_page=100&page={page}&status={status}',
+                headers=self.__auth_headers
+                )
             items = response['resources']
             if not items:
                 break
@@ -55,23 +61,25 @@ class AvitoApi:
 
     def __get_auth_headers(self, auth_data: dict) -> dict:
         access_token = self.__get_access_token(auth_data)
+        print(access_token)
         return {
+            'Authorization': f'Bearer {access_token.strip()}',
             'User-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
                           'Chrome/122.0.0.0 Safari/537.36',
-            'Authorization': f'Bearer {access_token}',
         }
 
-    def __get_access_token(self, auth_data: dict):
-        return self.__post(self.AUTH_API_HOST, data=auth_data).get('access_token')
+    def __get_access_token(self, auth_data: dict) -> str:
+        response = self.__post(self.AUTH_API_HOST, data=auth_data)
+        return response.get('access_token')
 
     def __get(self, url: str, params: dict|None = None, headers: dict|None = None) -> dict:
         return self.__request('GET', url, params, headers)
 
-    def __post(self, url: str, data: dict | None = None, headers: dict | None = None) -> dict:
-        return self.__request('POST', url, data, headers)
+    def __post(self, url: str, data: dict|str|None = None, headers: dict | None = None) -> dict:
+        return self.__request('POST', url, headers=headers, data=data)
 
     def __request(self, method: str, url: str, data: dict|None = None, headers: dict|None = None) -> dict:
-        response = requests.request(method, url, data=data, headers=headers)
+        response = requests.request(method, url, headers=headers, data=data)
         response_data = response.json()
         if response.status_code != 200 or response_data.get('error') is not None:
             raise Exception(f'Request to `{url}` failed with response: `{response_data}`')
