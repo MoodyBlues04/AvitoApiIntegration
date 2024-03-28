fc161c992b732a2990715886fe2df2ed36293bf2
MoodyBlues04 <105989507+MoodyBlues04@users.noreply.github.com>
2024-03-29 02:09:37 +0300

    дщпы

--- app/services/ads_sheet_processor.py
+++ app/services/ads_sheet_processor.py
@@ -20,28 +20,35 @@ class AvitoSheetProcessor:
                 if row_index == 0 or len(row) > 0 and not row[0]:
                     continue
                 if len(row) < self.ROW_LEN:
+                    print('Sheet finished')
                     break
 
                 profile_id, client_id, client_secret = row[1:4]
+                print(f'{row_index+1}. Processing profile: {profile_id} {client_id} {client_secret}')
 
                 auth_request = AuthRequest(client_id.strip(), client_secret.strip())
                 try:
                     avito_service = AvitoService(auth_request)
                 except Exception:
                     self.__set_status(row_index + 1, 'BLOCK')
+                    print('Profile blocked')
                     continue
+                print('Avito api authorized. Profile active')
 
                 self.__set_account_info(row_index, avito_service.get_account_info())
+                print('Account data updated')
 
                 ads_stat_by_region = avito_service.get_ads_stat_by_regions()
                 self.__update_ads_stat(profile_id, ads_stat_by_region)
+                print('Ads stats updated')
 
                 avito_service.answer_on_reviews()
+                print('Reviews answered')
 
                 # TODO cannot check if it works - no operations on test account
                 # operations_history = avito_service.api.get_month_operations_history()['result']['operations']
                 # self.__set_operation_history(profile_id, operations_history)
-
+                # print('Balance stats updated')
             except Exception as e:
                 self.__log_error(e, row_index)
 
